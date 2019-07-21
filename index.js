942e3909e71be61a00471437587735256d4b17da
Taha HussainAlash <taha.benzma94@gmail.com>
2019-07-21 03:46:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-/* Your code goes here */
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer(){
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'bob';
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'not bob';
+}
+
+let favoriteCustomer = 'bob';
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = 'not bob';
+
+  throw SyntaxError(SyntaxError)
+}
